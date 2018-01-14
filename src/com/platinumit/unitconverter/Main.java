35a30d4c780cac7715afd5008088cfcc32527779
platinumitpl <info@platinumit.pl>
2018-01-14 23:05:23 +0100

    switching scenes

--- src/com/platinumit/unitconverter/Main.java
+++ src/com/platinumit/unitconverter/Main.java
@@ -6,13 +6,17 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+import java.util.Stack;
 
 public class Main extends Application {
 
 
-    Button button;
-
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
 
@@ -26,36 +30,36 @@ public class Main extends Application {
 //
 //        System.out.println(convertedValue);
 
-        //go to Application and set up my JavaFX app
         launch(args);
 
     }
 
 
-    //Application will start "start" method
     @Override
     public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
 
-        //set title of primary stage
-        primaryStage.setTitle("Title of the window");
-        //create new button
-        button = new Button();
-        //set button label
-        button.setText("Convert");
-        button.setOnAction(e -> System.out.println(UnitConverter.convert(10, "kg", "lb")));
+        Label label1 = new Label("Welcome to the first scene");
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        //create new pane
-        StackPane layout = new StackPane();
-        //add button to the pane
-        layout.getChildren().add(button);
-        //create new scene (thing inside of a stage)
-        Scene scene = new Scene(layout, 300, 250);
-        //set scene in a stage
-        primaryStage.setScene(scene);
-        //show window (stage)
-        primaryStage.show();
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
 
+        scene1 = new Scene(layout1, 200, 200);
 
+        //Button 2
+        Button button2 = new Button("Go to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        //Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 200, 200);
+
+        window.setScene(scene1);
+        window.setTitle("Switching scenes");
+        window.show();
     }
 
 
