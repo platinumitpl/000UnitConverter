396641b509fef05adb10486fb84ca45fb6ad1236
platinumitpl <info@platinumit.pl>
2018-01-14 23:38:34 +0100

    switching scenes

--- src/com/platinumit/unitconverter/Main.java
+++ src/com/platinumit/unitconverter/Main.java
@@ -38,30 +38,23 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
-
-        Label label1 = new Label("Welcome to the first scene");
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction(e -> window.setScene(scene2));
-
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-
-        scene1 = new Scene(layout1, 200, 200);
-
-        //Button 2
-        Button button2 = new Button("Go to scene 1");
-        button2.setOnAction(e -> window.setScene(scene1));
-
-        //Layout 2
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2, 200, 200);
-
-        window.setScene(scene1);
         window.setTitle("Switching scenes");
+
+
+        Button button = new Button("New window");
+        button.setOnAction(e -> {
+
+            boolean result = ConfirmBox.display("Question", "Are you sure that you want to do this?");
+            System.out.println(result);
+        });
+
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 200, 200);
+        window.setScene(scene);
         window.show();
     }
-
 
 
 }
